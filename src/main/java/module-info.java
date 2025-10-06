73bd41350f653046688fdaaf0ce69ab4f48e1155
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-06 10:09:26 -0300

    feat: include fontawesome icons

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.proautokimium.alternarpowerbi {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+	requires java.desktop;
 
     opens com.proautokimium.alternarpowerbi to javafx.fxml;
     exports com.proautokimium.alternarpowerbi;
