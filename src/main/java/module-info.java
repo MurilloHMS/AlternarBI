c8575183f54905e4364d657d6676dcee2d7b9377
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-06 07:57:05 -0300

    feat: initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.proautokimium.alternarpowerbi {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.proautokimium.alternarpowerbi to javafx.fxml;
+    exports com.proautokimium.alternarpowerbi;
+}
