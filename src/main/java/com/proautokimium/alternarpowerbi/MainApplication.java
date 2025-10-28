7a7f97bd098b20501fc771fd9fecfc99597afd74
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-28 12:20:05 -0300

    feat: include new icons

--- src/main/java/com/proautokimium/alternarpowerbi/MainApplication.java
+++ src/main/java/com/proautokimium/alternarpowerbi/MainApplication.java
@@ -3,6 +3,7 @@ package com.proautokimium.alternarpowerbi;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +11,7 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+    	stage.getIcons().add(new Image(getClass().getResourceAsStream("assets/icon.png")));
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 450, 250);
         stage.setTitle("Alternar Power BI!");
